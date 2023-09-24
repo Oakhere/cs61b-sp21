855a39ba7e132bb5649a087e6853d3d92ef5ba44
Oak <2574438417@qq.com>
2023-09-24 21:07:15 +0800

    Fix the problem where the size of a LinkedListDeque becomes negative.

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -76,7 +76,7 @@ public class LinkedListDeque<T> {
     /** Removes and returns the item at the front of the deque.
      * If no such item exists, returns null. */
     public T removeFirst() {
-        if (sentinel.item == null) {
+        if (sentinel.item == null || size == 0) {
             return null;
         }
         T itemToReturn = first.item;
@@ -90,7 +90,7 @@ public class LinkedListDeque<T> {
     /** Removes and returns the item at the back of the deque.
     If no such item exists, returns null.*/
     public T removeLast() {
-        if (sentinel.item == null) {
+        if (sentinel.item == null || size == 0) {
             return null;
         }
         T itemToReturn = last.item;
