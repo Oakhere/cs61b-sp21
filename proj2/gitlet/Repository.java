159229c37ae86b00535a472d4e7943add8658c3a
Oak <2574438417@qq.com>
2023-12-03 16:43:01 +0800

    Modify merge conflict format. Gonna test it on autograder.

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -487,8 +487,8 @@ public class Repository {
                 } else {
                     contentInOther = Blob.getBlob(other.blobs.get(f)).contents;
                 }
-                String updatedContent = "<<<<<<< HEAD\n" + contentInHead + "=======\n"
-                        + contentInOther + ">>>>>>>";
+                String updatedContent = "<<<<<<< HEAD\n" + contentInHead + "\n=======\n"
+                        + contentInOther + "\n>>>>>>>";
                 writeContents(conflictFile, updatedContent);
                 add(f);
             }
